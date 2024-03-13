506b78618c4728c5dbf0381e468e004b19dbbb46
salomebubulashvili <Bubulashviliii@gmail.com>
2024-03-13 23:25:27 +0400

    add burger

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+"use stirct"
+
+const navBar = document.getElementById("nav");
+const burgerBar = document.getElementById("burgerBar");
+
+burgerBar.addEventListener("click", function() {
+    navBar.classList.toggle("activeNav");
+
+    const burgerLine2 = document.querySelector(".bar2"); 
+    burgerLine2.classList.toggle("burger-line-hide");
+
+    const burgerLines = document.querySelectorAll(".bar"); 
+    burgerLines.forEach(function (element) {
+        element.classList.toggle("lines-to-X");
+    });
+});
